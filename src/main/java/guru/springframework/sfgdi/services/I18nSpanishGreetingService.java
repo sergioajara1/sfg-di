eb8babef42b6e6a523882f9e8f851cae9080f11e
Sergio Jara <sergioajara@outlook.com>
2022-09-10 10:28:05 -0400

    End of Assignment 4 - Cleaned up files

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Profile({"ES","default"})
 @Service("i18nService")
-public class i18nSpanishGreetingService implements GreetingService{
+public class I18nSpanishGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
         return "Hola Mundo - ES";
